cb57a74256255b74408d06a1898531f65b3c3447
gaoph <123@163.com>
2023-12-04 06:40:24 +0800

    fisrt init

---
+++ src/main/java/com/example/demo2/Test123Controller.java
@@ -0,0 +1,11 @@
+package com.example.demo2;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Test123Controller {
+
+
+}
