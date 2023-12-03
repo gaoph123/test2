d042229e4e217f54e1a87cbefd2d53ad8162ae05
gaoph <123@163.com>
2023-12-04 07:59:17 +0800

    three init

--- src/main/java/com/example/demo2/Test123Controller.java
+++ src/main/java/com/example/demo2/Test123Controller.java
@@ -1,5 +1,6 @@
 package com.example.demo2;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class Test123Controller {
 
-
+    @GetMapping
+    public String test1(){
+        return "成功";
+    }
 }
